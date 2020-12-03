7efb1a33c8f2baa7188fe0039470daca95fdc877
R Home <74921709+RNairn01@users.noreply.github.com>
2020-12-03 18:51:03 +0000

    Added gm rolling

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const COMMAND = '!';
 const rollPrefix = 'roll';
 const shortRollPrefix = 'r';
 const helpPrefix = 'help';
+const gmRollPrefix = 'gm';
 
 client.on('ready', () => {
     console.log('Logged in!');
@@ -19,7 +20,8 @@ client.on('message', msg => {
 
    if (msg.content === `${COMMAND}${helpPrefix}` && !msg.author.bot) {
     msg.reply('\n**COMMANDS:** \n\n' + '**!help** - display this help info.\n\n' + '**!roll** or **!r** - roll the specified number of a certain dice type. Can roll multiple dice from one command by seperating inputs with spaces.\n ' + 
-    'e.g. "!roll 4g 3p" will roll 4 green ability dice and 3 purple difficulty dice.\n\n' + 
+    'e.g. "!roll 4g 3p" will roll 4 green ability dice and 3 purple difficulty dice.\n\n' +
+    '**!gm** - takes same inputs as !roll command but DMs the result to the user for secret rolls.\n\n' + 
     '**Dice Types:** \ny - yellow proficiency die \ng - green ability die \nr - red challenge die \np - purple difficulty die \nb - boost die \ns - setback die \nf - force die (SW Only)'); 
    }
    
@@ -57,6 +59,36 @@ client.on('message', msg => {
 
    if(fInput > 0) msg.reply(dice.forceOutput(fInput));
  };
-});
+ 
+  if (msg.content.startsWith(COMMAND + gmRollPrefix) && !msg.author.bot) {
+  
+    let args = msg.content.slice(gmRollPrefix.length + COMMAND.length).trim().split(/ +/);
 
+    let yInput = 0;
+    let gInput = 0;
+    let rInput = 0;
+    let pInput = 0;
+    let bInput = 0;
+    let sInput = 0;
+    let fInput = 0;
+ 
+    args.forEach(e => {
+     if (e[1] == 'y') yInput = e[0];
+     if (e[1] == 'g') gInput = e[0];
+     if (e[1] == 'r') rInput = e[0];
+     if (e[1] == 'p') pInput = e[0];
+     if (e[1] == 'b') bInput = e[0];
+     if (e[1] == 's') sInput = e[0];
+     if (e[1] == 'f') fInput = e[0];
+    });
+
+    let rollInputs = [yInput, gInput, rInput, pInput, bInput, sInput];
+ 
+    if (rollInputs.some(e => e > 0)) {
+    msg.author.send(dice.determineSuccess(yInput, gInput, rInput, pInput, bInput, sInput));
+    };
+
+    if(fInput > 0) msg.author.send(dice.forceOutput(fInput));
+ };
+});
 client.login(process.env.BOT_TOKEN)
